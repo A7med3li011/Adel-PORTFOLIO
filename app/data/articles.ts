5dcf6b714fd007f4c2878997d97dc25a19e46fb2
3delmostafa <216431419+3delmostafa@users.noreply.github.com>
2026-07-31 16:18:27 +0300

    feat: remove readTime from articles and add new article on customizable media picker

--- app/data/articles.ts
+++ app/data/articles.ts
@@ -3,7 +3,6 @@ export type ArticleItem = {
   title: string;
   thumbnail: string;
   url: string;
-  readTime: string;
   category: string;
 };
 
@@ -13,7 +12,6 @@ export const articles: ArticleItem[] = [
     title: "From 21s to 7s: Optimizing a Zero-Knowledge Authentication Flow",
     thumbnail: "/assets/images/articles/article1.jpg",
     url: "https://medium.com/@adelmostafamohamed12/from-21s-to-7s-optimizing-a-zero-knowledge-authentication-flow-7dee3ffd9115",
-    readTime: "8 min read",
     category: "Performance",
   },
   {
@@ -22,7 +20,14 @@ export const articles: ArticleItem[] = [
       "Zero-Knowledge Encryption, The Service Provider Can’t Read Your Data",
     thumbnail: "/assets/images/articles/article2.png",
     url: "https://medium.com/@adelmostafamohamed12/zero-knowledge-encryption-c24168f24752?sharedUserId=adelmostafamohamed12",
-    readTime: "8 min read",
     category: "Security",
+  },
+  {
+    id: 3,
+    title:
+      "Building a Fully Customizable Media Picker for Flutter (Android & iOS)",
+    thumbnail: "/assets/images/articles/article3.png",
+    url: "https://medium.com/@adelmostafamohamed12/building-a-fully-customizable-media-picker-for-flutter-android-ios-28cf551e4865?sharedUserId=adelmostafamohamed12",
+    category: "Development",
   },
 ];
